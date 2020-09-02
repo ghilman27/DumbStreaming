063214e0af8a519fde1791d372329eeba0db7e6a
Ghilman AL Fatih <ghilman27@gmail.com>
2020-09-02 13:18:17 +0700

    specify DB logging for demonstration

--- DumbStreamingAPI/config/database.js
+++ DumbStreamingAPI/config/database.js
@@ -9,6 +9,7 @@ const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     operatorAliases: false,
+    logging: process.env.DB_LOG || false,
     pool: {
         max: 5,
         min: 0,
